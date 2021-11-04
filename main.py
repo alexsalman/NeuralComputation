64d9399a34a6969bdbd15101b5256544c4795024
Alex Salman <aliayadalsalman@gmail.com>
2021-11-03 20:26:54 -0700

    Update

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-# get softmax output
+
 import torch
 import load_pretrained_models
 import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = ''
-# print(torch.cuda.is_available())
-# softmax_output = SomeCNN(input_image) # replace with your favourite CNN
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+print(torch.cuda.is_available())
+softmax_output = SomeCNN(input_image) # replace with your favourite CNN
 
-# convert to numpy
-# softmax_output_numpy = SomeConversionToNumpy(softmax_output) # replace with conversion
+convert to numpy
+softmax_output_numpy = SomeConversionToNumpy(softmax_output) # replace with conversion
 
-# create mapping
-# mapping = probabilities_to_decision.ImageNetProbabilitiesTo16ClassesMapping()
+create mapping
+mapping = probabilities_to_decision.ImageNetProbabilitiesTo16ClassesMapping()
 
-# obtain decision
-# decision_from_16_classes = mapping.probabilities_to_decision(softmax_output_numpy)
+obtain decision
+decision_from_16_classes = mapping.probabilities_to_decision(softmax_output_numpy)
 
-import subprocess
-subprocess.run(["scp", 'load_pretrained_models.py', "alexsalman@deepcore.soe.ucsc.edu:/soe/alexsalman"])
+#import subprocess
+#subprocess.run(["scp", 'load_pretrained_models.py', "alexsalman@deepcore.soe.ucsc.edu:/soe/alexsalman"])
